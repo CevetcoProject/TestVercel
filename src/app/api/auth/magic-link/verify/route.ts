265a682d7d80da7d1039e465f9b706f492081d9e
lareinne <133171077+lareinne@users.noreply.github.com>
2025-01-10 17:22:53 +0100

    Update route.ts

--- src/app/api/auth/magic-link/verify/route.ts
+++ src/app/api/auth/magic-link/verify/route.ts
@@ -5,5 +5,14 @@ import { User } from "@/models/User";
 
 connect();
 
+export async function POST(request: NextRequest) {
+  const payload: MagicLinkPayloadVerify = await request.json();
+
+  return NextResponse.json({
+    status: 200,
+    message: "Link is valid.",
+    email: email,
+  });
+}
  
 
