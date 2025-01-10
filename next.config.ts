9f42b137a68f2922d700ee02b6ae1627cb0e1dad
lareinne <133171077+lareinne@users.noreply.github.com>
2025-01-10 17:00:50 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint pendant la phase de build
+  },
+  // Ajoutez d'autres options de configuration si nécessaire
 };
 
 export default nextConfig;
