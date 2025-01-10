5d03da2a3bca21772bc25973c4ed9db5913f84ba
lareinne <133171077+lareinne@users.noreply.github.com>
2025-01-10 17:17:31 +0100

    Update route.ts

--- src/app/api/auth/magic-link/verify/route.ts
+++ src/app/api/auth/magic-link/verify/route.ts
@@ -1,1 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+import Env from "@/config/env";
+import { connect } from "@/database/mongo.config";
+import { User } from "@/models/User";
+
+connect();
+
+ 
 
