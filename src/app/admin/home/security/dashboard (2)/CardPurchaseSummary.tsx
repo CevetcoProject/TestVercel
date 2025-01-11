1809c8c628364d485673e38e0c4b584a90ea0286
lareinne <133171077+lareinne@users.noreply.github.com>
2025-01-11 07:54:19 +0100

    Update CardPurchaseSummary.tsx

--- src/app/admin/home/security/dashboard (2)/CardPurchaseSummary.tsx
+++ src/app/admin/home/security/dashboard (2)/CardPurchaseSummary.tsx
@@ -5,11 +5,8 @@ const CardPurchaseSummary = () => {
   
   return (
     <div>
-              <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
-                Sales Summary
-              </h2>
-              <hr />
-          </div>
+      <h1>Stat Card</h1>
+    </div>
   );
 };
 
