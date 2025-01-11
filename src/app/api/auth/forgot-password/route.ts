3a254e766b934e744f5ab2296bc0ac9e22797867
lareinne <133171077+lareinne@users.noreply.github.com>
2025-01-11 08:00:44 +0100

    Update route.ts

--- src/app/api/auth/forgot-password/route.ts
+++ src/app/api/auth/forgot-password/route.ts
@@ -1,11 +1,4 @@
-import { User } from "@/models/User";
-import { NextRequest, NextResponse } from "next/server";
 
-import Cryptr from "cryptr";
-import Env from "@/config/env";
-import { render } from "@react-email/render";
-import ForgotPasswordEmail from "@/emails/ForgotPasswordEmail";
-import { sendEmail } from "@/config/mail";
 import { connect } from "@/database/mongo.config";
 
 connect();
