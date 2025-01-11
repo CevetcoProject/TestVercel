8fbba01dae31e9d94ea6d64930bb953f520b0ba5
lareinne <133171077+lareinne@users.noreply.github.com>
2025-01-11 07:55:18 +0100

    Update CardExpenseSummary.tsx

--- src/app/admin/home/security/dashboard (2)/CardExpenseSummary.tsx
+++ src/app/admin/home/security/dashboard (2)/CardExpenseSummary.tsx
@@ -1,12 +1,10 @@
 const CardExpenseSummary = () => {
 
-  return (
+ return (
     <div>
-              <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
-                Sales Summary
-              </h2>
-              <hr />
+      <h1>Stat Card</h1>
     </div>
+  );
 };
 
 export default CardExpenseSummary;
