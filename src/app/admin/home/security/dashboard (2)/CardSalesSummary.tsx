11642d4af360858961c909a7d401ceb2572eb026
lareinne <133171077+lareinne@users.noreply.github.com>
2025-01-11 07:53:51 +0100

    Update CardSalesSummary.tsx

--- src/app/admin/home/security/dashboard (2)/CardSalesSummary.tsx
+++ src/app/admin/home/security/dashboard (2)/CardSalesSummary.tsx
@@ -4,14 +4,10 @@ import React, { useState } from "react";
 
 const CardSalesSummary = () => {
 
-  return (
-          <div>
-              <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
-                Sales Summary
-              </h2>
-              <hr />
-          </div>
-  
+ return (
+    <div>
+      <h1>Stat Card</h1>
+    </div>
   );
 };
 
