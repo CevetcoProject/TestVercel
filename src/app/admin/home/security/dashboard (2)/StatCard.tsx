f7e6e34df67105408ee946bd4be0e644ba468185
lareinne <133171077+lareinne@users.noreply.github.com>
2025-01-11 06:40:27 +0100

    Update StatCard.tsx

--- src/app/admin/home/security/dashboard (2)/StatCard.tsx
+++ src/app/admin/home/security/dashboard (2)/StatCard.tsx
@@ -1,22 +1,11 @@
-import { LucideIcon } from "lucide-react";
-import React from "react";
+import React from 'react';
 
-const StatCard = () =>
-
-    return (
-      <div className="md:row-span-1 xl:row-span-2 bg-white col-span-1 shadow-md rounded-2xl flex flex-col justify-between">
-        {/* HEADER */}
-        <div>
-          <div className="flex justify-between items-center mb-2 px-5 pt-4">
-            <h2 className="font-semibold text-lg text-gray-700">{title}</h2>
-            <span className="text-xs text-gray-400">{dateRange}</span>
-          </div>
-          <hr />
-        </div>
-  
-      </div>
-    );
-
+const StatCard: React.FC = () => {
+  return (
+    <div>
+      <h1>Stat Card</h1>
+    </div>
+  );
 };
 
 export default StatCard;
